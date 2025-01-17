25a82ac924933f4e1bfccfd5634b982524cf134e
Liam Appleton <liamappleton97@outlook.com>
2025-01-17 10:06:58 +0000

    refactored checkCurrenciesSelected function add added dynamic styling to inputOne

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ const currencyTwo = document.getElementById('curr-two');
 const inputOne = document.getElementById('input-one');
 const inputTwo = document.getElementById('input-two');
 
-let { have, want, amount, newAmount, rate, checkCurrenciesSelected } = {
+let { have, want, amount, newAmount, rate } = {
   have: '',
   want: '',
   amount: 0,
   newAmount: 0,
   rate: 0,
-  checkCurrenciesSelected: () => {
-    return currencyOne.value && currencyTwo.value ? true : false;
-  },
+};
+
+const checkCurrenciesSelected = () => {
+  if (currencyOne.value && currencyTwo.value) {
+    inputOne.disabled = false;
+    inputOne.classList.add('input-one-active');
+  } else {
+    inputOne.disabled = true;
+    inputOne.classList.remove('input-one-active');
+  }
 };
 
 const fetchData = async () => {
@@ -31,38 +38,36 @@ const fetchData = async () => {
   }
 };
 
-currencyOne.addEventListener('change', async (e) => {
+currencyOne.addEventListener('change', (e) => {
   have = e.target.value;
 
-  if (checkCurrenciesSelected()) inputOne.disabled = false;
-  else inputOne.disabled = true;
+  checkCurrenciesSelected();
 });
 
 currencyTwo.addEventListener('change', async (e) => {
   want = e.target.value;
 
   const { data } = await fetchData();
-  rate = parseFloat(data[want]).toFixed(2);
+  rate = parseFloat(data[want]);
+  console.log(rate);
 
-  if (checkCurrenciesSelected()) inputOne.disabled = false;
-  else inputOne.disabled = true;
+  checkCurrenciesSelected();
 });
 
 inputOne.addEventListener('input', (e) => {
-  let userInput = parseFloat(e.target.value);
-  console.log(userInput);
-
+  let userInput = e.target.value;
+  // prevent user from entering more than 2 dp
+  if (userInput.match(/^.*\.\d{3,}$/)) {
+    e.target.value = userInput.substring(0, userInput.length - 1);
+  }
+  userInput = parseFloat(e.target.value).toFixed(2);
+  // prevent user from entering negative numbers
   if (isNaN(userInput)) {
     userInput = 0;
     inputOne.value = '';
   }
 
   amount = userInput;
-
   newAmount = amount * rate;
-
-  console.log('amount: ' + amount);
-  console.log('new: ' + newAmount);
-
-  inputTwo.value = newAmount;
+  inputTwo.value = newAmount.toFixed(2);
 });
