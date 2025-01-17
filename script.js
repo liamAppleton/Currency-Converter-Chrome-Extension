d964e659f6e8db0e7e94e5af4330a689d8ed7caa
Liam Appleton <liamappleton97@outlook.com>
2025-01-17 00:25:52 +0000

    added dynamic output based on user input. also prevented inputs if one or both currencies are not selected

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 const currencyOne = document.getElementById('curr-one');
 const currencyTwo = document.getElementById('curr-two');
 const inputOne = document.getElementById('input-one');
-const inputTwo = document.getElementById('input-one');
+const inputTwo = document.getElementById('input-two');
 
-let { have, want, amount } = {
+let { have, want, amount, newAmount, rate, checkCurrenciesSelected } = {
   have: '',
   want: '',
   amount: 0,
+  newAmount: 0,
+  rate: 0,
+  checkCurrenciesSelected: () => {
+    return currencyOne.value && currencyTwo.value ? true : false;
+  },
 };
 
-const apiKey = 'bWbwxDdtS9Bzsouymirguw==uZhWEMRD79j4eKVg';
-const url = `https://api.api-ninjas.com/v1/convertcurrency?have=${'GBP'}&want=${'AUD'}&amount=${5000}`;
-
 const fetchData = async () => {
+  const apiKey = 'bWbwxDdtS9Bzsouymirguw==uZhWEMRD79j4eKVg';
+  const url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_qzfbQmDcpUcEM0uUnqewmns7EoTDtLA9C30pSRXt&currencies=${want}&base_currency=${have}`;
+
   try {
-    const response = await fetch(url, {
-      headers: {
-        'X-Api-Key': apiKey,
-      },
-    });
+    const response = await fetch(url);
 
     if (!response.ok) {
       console.log(`Error: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.log(error.message);
   }
 };
 
-currencyOne.addEventListener('change', (e) => {
+currencyOne.addEventListener('change', async (e) => {
   have = e.target.value;
+
+  if (checkCurrenciesSelected()) inputOne.disabled = false;
+  else inputOne.disabled = true;
 });
 
-currencyTwo.addEventListener('change', (e) => {
+currencyTwo.addEventListener('change', async (e) => {
   want = e.target.value;
+
+  const { data } = await fetchData();
+  rate = parseFloat(data[want]).toFixed(2);
+
+  if (checkCurrenciesSelected()) inputOne.disabled = false;
+  else inputOne.disabled = true;
+});
+
+inputOne.addEventListener('input', (e) => {
+  let userInput = parseFloat(e.target.value);
+
+  if (isNaN(userInput)) userInput = 0;
+
+  amount = userInput;
+
+  newAmount = amount * rate;
+
+  console.log('amount: ' + amount);
+  console.log('new: ' + newAmount);
+
+  inputTwo.value = newAmount;
 });
